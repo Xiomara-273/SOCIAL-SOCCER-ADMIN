42c352e2c80de51d3d84672e82901d2e1f2ebdc1
Xiomara-273 <xiomy.m34.12@gmailo.com>
2026-01-06 13:32:53 -0500

    Mis últimos cambios en el main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { TournamentAdminComponent } from './modules/tournaments/pages/tournament-admin/tournament-admin';
 import { EnrollmentAdminComponent } from './modules/tournaments/pages/enrollment-admin/enrollment-admin';
 import { TournamentDashboardComponent } from './modules/tournaments/pages/tournament-dashboard/tournament-dashboard';
+import { RoleAdminComponent } from './modules/tournaments/components/role-admin/role-admin.component';
 
 export const routes: Routes = [
+  // 1. Las rutas específicas siempre van ARRIBA
+  { path: 'roles-admin', component: RoleAdminComponent },
   { path: 'admin/tournaments', component: TournamentAdminComponent },
   { path: 'admin/enrollments', component: EnrollmentAdminComponent },
   { path: 'dashboard', component: TournamentDashboardComponent }, 
+  
+  // 2. Las redirecciones y comodines siempre van AL FINAL
   { path: '', redirectTo: 'admin/tournaments', pathMatch: 'full' },
-  { path: '**', redirectTo: 'admin/tournaments' } 
+  { path: '**', redirectTo: 'admin/tournaments' }
 ];
